961d40de30e54bae4460154aee348348508e71e2
DebuggerOR <foglerori@gmail.com>
2021-06-02 23:51:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,85 @@
+import torch
+from torch import nn
+
+import math
+import matplotlib.pyplot as plt
+
+from discriminator import Discriminator
+from generator import Generator
+from generate_data import get_data
+
+
+def train(train_set, generator, discriminator):
+    batch_size = 32
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=batch_size, shuffle=True
+    )
+
+    lr = 0.001
+    num_epochs = 300
+    loss_function = nn.BCELoss()
+
+    optimizer_discriminator = torch.optim.Adam(discriminator.parameters(), lr=lr)
+    optimizer_generator = torch.optim.Adam(generator.parameters(), lr=lr)
+
+    for epoch in range(num_epochs):
+        for n, (real_samples, _) in enumerate(train_loader):
+            # Data for training the discriminator
+            real_samples_labels = torch.ones((batch_size, 1))
+            latent_space_samples = torch.randn((batch_size, 2))
+            generated_samples = generator(latent_space_samples)
+            generated_samples_labels = torch.zeros((batch_size, 1))
+            all_samples = torch.cat((real_samples, generated_samples))
+            all_samples_labels = torch.cat(
+                (real_samples_labels, generated_samples_labels)
+            )
+
+            # Training the discriminator
+            discriminator.zero_grad()
+            output_discriminator = discriminator(all_samples)
+            loss_discriminator = loss_function(
+                output_discriminator, all_samples_labels)
+            loss_discriminator.backward()
+            optimizer_discriminator.step()
+
+            # Data for training the generator
+            latent_space_samples = torch.randn((batch_size, 2))
+
+            # Training the generator
+            generator.zero_grad()
+            generated_samples = generator(latent_space_samples)
+            output_discriminator_generated = discriminator(generated_samples)
+            loss_generator = loss_function(
+                output_discriminator_generated, real_samples_labels
+            )
+            loss_generator.backward()
+            optimizer_generator.step()
+
+            # Show loss
+            if epoch % 10 == 0 and n == batch_size - 1:
+                print(f"Epoch: {epoch} Loss D.: {loss_discriminator}")
+                print(f"Epoch: {epoch} Loss G.: {loss_generator}")
+
+
+if __name__ == '__main__':
+    torch.manual_seed(111)
+
+    train_data_length = 1024
+    train_data = get_data(train_data_length, 'spiral')
+    train_data=train_data.astype(float)
+    train_labels = torch.zeros(train_data_length)
+
+    train_set = [
+        (train_data[i], train_labels[i]) for i in range(train_data_length)
+    ]
+
+    generator = Generator()
+    discriminator = Discriminator()
+
+    train(train_set, generator, discriminator)
+
+    latent_space_samples = torch.randn(100, 2)
+    generated_samples = generator(latent_space_samples)
+
+    generated_samples = generated_samples.detach()
+    plt.plot(generated_samples[:, 0], generated_samples[:, 1], ".")
